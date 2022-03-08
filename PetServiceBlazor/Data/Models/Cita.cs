fcebf05df9fe0520659552aff6c6736a06724d48
BOT003945 <76027806+BOT003945@users.noreply.github.com>
2022-03-07 21:13:33 -0600

    Add files via upload

---
+++ PetServiceBlazor/Data/Models/Cita.cs
@@ -0,0 +1,18 @@
+﻿using PetServiceBlazor.Data.Models;
+using System;
+using System.Collections.Generic;
+
+namespace PetServiceBlazor.Data.Models
+{
+    public partial class Cita
+    {
+        public int IdCitas { get; set; }
+        public int IdPropietario { get; set; }
+        public int IdPerro { get; set; }
+        public DateTime Fecha { get; set; }
+        public string? Descripcion { get; set; }
+
+        public virtual Mascota IdPerroNavigation { get; set; } = null!;
+        public virtual Propietario IdPropietarioNavigation { get; set; } = null!;
+    }
+}
---
+++ PetServiceBlazor/Data/Models/Usuario.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using PetServiceBlazor.Data.Models;
+
+
+namespace PetServiceBlazor.Data.Models
+{
+    public partial class Usuario
+    {
+        public Usuario()
+        {
+            Propietarios = new HashSet<Propietario>();
+        }
+
+        public int IdUsuario { get; set; }
+        public string? Correo { get; set; }
+        public string? FotoUsuario { get; set; }
+        public string Usuario1 { get; set; } = null!;
+        public string Contraseña { get; set; } = null!;
+        public string Rol { get; set; } = null!;
+        public DateTime FechaRegistro { get; set; }
+
+        public virtual ICollection<Propietario> Propietarios { get; set; }
+    }
+}
