46b9426d926781d6a3a141b58d065d59365e55be
BOT003945 <76027806+BOT003945@users.noreply.github.com>
2022-03-15 09:43:19 -0600

    Add files via upload

---
+++ PetService/Models/Usuario.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PetService.Models
+{
+    public partial class Usuario
+    {
+        public Usuario()
+        {
+            Propietarios = new HashSet<Propietario>();
+        }
+
+        public int IdUsuario { get; set; }
+        public string? Correo { get; set; }
+        public string? FotoUsuario { get; set; }
+        public string Usuario1 { get; set; } = null!;
+        public string Contraseña { get; set; } = null!;
+        public string Rol { get; set; } = null!;
+        public DateTime FechaRegistro { get; set; }
+
+        public virtual ICollection<Propietario> Propietarios { get; set; }
+    }
+}
