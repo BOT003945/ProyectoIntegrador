155e800ce05c68d28b80e291e610a399c9f3ce2c
BOT003945 <76027806+BOT003945@users.noreply.github.com>
2022-03-15 00:01:46 -0600

    Add files via upload

---
+++ PetService/Models/Servicio.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PetService.Models
+{
+    /// <summary>
+    /// Tabla Servicio, donde se almacenan los diferentes tipos de servicios.
+    /// </summary>
+    public partial class Servicio
+    {
+        public Servicio()
+        {
+            Cita = new HashSet<Cita>();
+        }
+
+        /// <summary>
+        /// Campo IdServicio, llave primaria en la tabla Servicio.
+        /// </summary>
+        public int IdServicio { get; set; }
+        public string NombreServicio { get; set; } = null!;
+
+        public virtual ICollection<Cita> Cita { get; set; }
+    }
+}
