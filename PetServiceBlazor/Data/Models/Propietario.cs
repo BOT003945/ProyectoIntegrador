77ed50e8027d9c8f3bd37120a7bb25f7eb896696
BOT003945 <76027806+BOT003945@users.noreply.github.com>
2022-03-15 12:51:30 -0600

    Add files via upload

---
+++ PetServiceBlazor/Data/Models/Propietario.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PetServiceBlazor.Data.Models
+{
+    public partial class Propietario
+    {
+        public Propietario()
+        {
+            Mascota = new HashSet<Mascota>();
+        }
+
+        public int IdPropietario { get; set; }
+        /// <summary>
+        /// Campo Nombres, indica uno o los dos nombres de las posibles usuarios.
+        /// </summary>
+        public string Nombres { get; set; } = null!;
+        /// <summary>
+        /// Campo ApellidoP, indica el apellido paterno del usuario.
+        /// </summary>
+        public string ApellidoP { get; set; } = null!;
+        /// <summary>
+        /// Campo ApellidoM, indica el apellido materno del usuario.
+        /// </summary>
+        public string ApellidoM { get; set; } = null!;
+        /// <summary>
+        /// Campo FechaNacimiento, indica su cumpleaños para calcular edad.
+        /// </summary>
+        public DateTime FechaNacimiento { get; set; }
+        /// <summary>
+        /// Campo Telefono, indica el telefono del usuario.
+        /// </summary>
+        public string Telefono { get; set; } = null!;
+        /// <summary>
+        /// Campo Sexo, indica el sexo del usuario.
+        /// </summary>
+        public string? Sexo { get; set; }
+        public string? Direccion { get; set; }
+        public int IdUsuario { get; set; }
+
+        public string? GetName()
+        {
+            return $"{Nombres} {ApellidoP} {ApellidoM}";
+        }
+
+        public virtual Usuario IdUsuarioNavigation { get; set; } = null!;
+        public virtual ICollection<Mascota> Mascota { get; set; }
+    }
+}
