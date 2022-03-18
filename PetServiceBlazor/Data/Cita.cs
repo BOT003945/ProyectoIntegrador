11adf9885bca470f68d08f0f1aa1486d7a27a6da
BOT003945 <76027806+BOT003945@users.noreply.github.com>
2022-03-17 20:57:07 -0600

    Add files via upload

---
+++ PetServiceBlazor/Data/Cita.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PetService.Models
+{
+    /// <summary>
+    /// Tabla citas, donde se asigna el id de la mascota y el propietario para cada servicio; por supuesto para asignación 
+    /// </summary>
+    public partial class Cita
+    {
+        /// <summary>
+        /// Campo IdCita es el identificador utilizado para cada cita.
+        /// </summary>
+        public int IdCita { get; set; }
+        /// <summary>
+        /// Campo IdServicio, llave foránea que conecta con la tabla de servicios para asignarse en cada cita.
+        /// </summary>
+        public int IdServicio { get; set; }
+        /// <summary>
+        /// Campo IdUsuario, llave foránea que conecta con la tabla de usuarios que corresponde al dueño del perro.
+        /// </summary>
+        public int IdUsuario { get; set; }
+        /// <summary>
+        /// Campo IdMascota, llave foránea que conecta con la tabla de mascotas que corresponde al perro de la persona.
+        /// </summary>
+        public int IdMascota { get; set; }
+        /// <summary>
+        /// Campo Fecha, atributo de tiempo para designar el día de la cita.
+        /// </summary>
+        public DateTime Fecha { get; set; }
+        /// <summary>
+        /// Campo HoraInicial, atributo para designar la hora de la cita.
+        /// </summary>
+        public DateTime? HoraInicial { get; set; }
+        /// <summary>
+        /// Campo Descripcion, atributo para designar el motivo de cita y/o lo que se va a hacer.
+        /// </summary>
+        public string? Descripcion { get; set; }
+
+        public virtual Mascota IdMascotaNavigation { get; set; } = null!;
+        public virtual Servicio IdServicioNavigation { get; set; } = null!;
+        public virtual Usuario IdUsuarioNavigation { get; set; } = null!;
+    }
+}
---
+++ PetServiceBlazor/Data/Producto.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PetService.Models
+{
+    /// <summary>
+    /// Tabla Producto, donde se almacena la información de cada producto a la venta.
+    /// </summary>
+    public partial class Producto
+    {
+        public Producto()
+        {
+            VentaDetalles = new HashSet<VentaDetalle>();
+        }
+
+        /// <summary>
+        /// Campo IdProductos, llave primaria en la tabla de productos.
+        /// </summary>
+        public int IdProductos { get; set; }
+        /// <summary>
+        /// Campo FotoProducto, guarda la información (URL) para una foto del producto.
+        /// </summary>
+        public string? FotoProducto { get; set; }
+        /// <summary>
+        /// Campo NombreProducto, indica el nombre producto.
+        /// </summary>
+        public string NombreProducto { get; set; } = null!;
+        /// <summary>
+        /// Campo Descripcion, da una descripción de lo que hace o es el artículo.
+        /// </summary>
+        public string? Descripcion { get; set; }
+        /// <summary>
+        /// Campo Precio, precio del producto.
+        /// </summary>
+        public double Precio { get; set; }
+        /// <summary>
+        /// Campo Inventario, cantidad del producto disponible.
+        /// </summary>
+        public int Inventario { get; set; }
+
+        public virtual ICollection<VentaDetalle> VentaDetalles { get; set; }
+    }
+}
