434a1d9cfb4716c8f8d15bc5bd61eaae95523029
BOT003945 <76027806+BOT003945@users.noreply.github.com>
2022-03-18 00:59:10 -0600

    Add files via upload

---
+++ PetServiceBlazor/Data/Models/Venta.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PetServiceBlazor.Data.Models
+{
+    public partial class Venta
+    {
+        public Venta()
+        {
+            VentaDetalles = new HashSet<VentaDetalle>();
+        }
+
+        /// <summary>
+        /// Campo IdVenta, llave primaria en la tabla Venta.
+        /// </summary>
+        public int IdVenta { get; set; }
+        /// <summary>
+        /// Campo IdUsuario, llave foránea que conecta con la tabla Usuario.
+        /// </summary>
+        public int IdUsuario { get; set; }
+        /// <summary>
+        /// Campo Fecha, campo que guarda la fecha efectuada de la venta.
+        /// </summary>
+        public DateTime Fecha { get; set; }
+
+        public virtual Usuario IdUsuarioNavigation { get; set; } = null!;
+        public virtual ICollection<VentaDetalle> VentaDetalles { get; set; }
+    }
+}
---
+++ PetServiceBlazor/Data/Models/VentaDetalle.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PetServiceBlazor.Data.Models
+{
+    public partial class VentaDetalle
+    {
+        /// <summary>
+        /// Campo IdVentaDetalle, llave primaria en la tabla Venta_Detalle.
+        /// </summary>
+        public int IdVentaDetalle { get; set; }
+        /// <summary>
+        /// Campo IdVenta, llave foránea que conecta con la tabla Venta.
+        /// </summary>
+        public int IdVenta { get; set; }
+        /// <summary>
+        /// Campo IdProducto, llave foránea que conecta con la tabla Producto.
+        /// </summary>
+        public int IdProducto { get; set; }
+        /// <summary>
+        /// Campo Descripcion, campo que describe el detalle del registro.
+        /// </summary>
+        public string Descripcion { get; set; } = null!;
+        /// <summary>
+        /// Campo Cantidad, campo que describe la cantidad de algún producto comprado.
+        /// </summary>
+        public int? Cantidad { get; set; }
+        /// <summary>
+        /// Campo Costo, campo que da el precio del producto, servicio, cita... Etc, al usuario.
+        /// </summary>
+        public double Costo { get; set; }
+
+        public virtual Producto IdProductoNavigation { get; set; } = null!;
+        public virtual Venta IdVentaNavigation { get; set; } = null!;
+    }
+}
