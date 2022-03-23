b26d912b068d090e8e3eeddc10ed8dd2641eed58
BOT003945 <76027806+BOT003945@users.noreply.github.com>
2022-03-22 22:41:30 -0600

    Add files via upload

---
+++ PetServiceBlazor/Startup.cs
@@ -0,0 +1,16 @@
+/*using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+//using PetServiceBlazor.Data.Auth;
+
+namespace PetServiceBlazor;
+public class Startup{
+    public void ConfigureServices(IServiceCollection services){
+        services.AddAuthorizationCore();
+        services.AddScoped<AuthenticationStateProvider, AuthStateProviderFalse>();
+    }
+    //public void Configure(IComponentsApplicationBuilder app){
+    //    app.AddComponent<App>("app");
+    //}
+}
+*/
