4012d6c251024f1ee1b37baaf1fcea9ff31c4f68
BOT003945 <76027806+BOT003945@users.noreply.github.com>
2022-03-22 22:35:05 -0600

    Add files via upload

---
+++ PetServiceBlazor/Data/Models/Usuario.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PetServiceBlazor.Data.Models
+{
+    public partial class Usuario
+    {
+        public Usuario()
+        {
+            Mascota = new HashSet<Mascota>();
+        }
+
+        public int IdUsuario { get; set; }
+        public string Nombre { get; set; } = null!;
+        public string ApellidoP { get; set; } = null!;
+        public string? ApellidoM { get; set; }
+        public int IdRol { get; set; }
+
+        public string? GetName()
+        {
+            return $"{Nombre} {ApellidoP} {ApellidoM}";
+        }
+
+        public virtual Rol IdRolNavigation { get; set; } = null!;
+        public virtual ICollection<Mascota> Mascota { get; set; }
+    }
+}
